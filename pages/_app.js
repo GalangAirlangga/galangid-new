e3234da2cdb66bf3b428e92e0a2a1968c3f6c001
Galang Airlangga <galangairlangga1@gmail.com>
2023-02-23 10:05:03 +0700

    error in analitics

--- pages/_app.js
+++ pages/_app.js
@@ -1,14 +1,14 @@
 import '@/css/tailwind.css'
 import '@/css/prism.css'
 import 'katex/dist/katex.css'
-import { AnalyticsVercel } from '@vercel/analytics/react'
+import { Analytics } from '@vercel/analytics/react'
 import '@fontsource/outfit'
 
 import { ThemeProvider } from 'next-themes'
 import Head from 'next/head'
 
 import siteMetadata from '@/data/siteMetadata'
-import Analytics from '@/components/analytics'
+import AnalyticsLocal from '@/components/analytics'
 import LayoutWrapper from '@/components/LayoutWrapper'
 import { ClientReload } from '@/components/ClientReload'
 
@@ -22,11 +22,11 @@ export default function App({ Component, pageProps }) {
         <meta content="width=device-width, initial-scale=1" name="viewport" />
       </Head>
       {isDevelopment && isSocket && <ClientReload />}
+      <AnalyticsLocal />
       <Analytics />
       <LayoutWrapper>
         <Component {...pageProps} />
       </LayoutWrapper>
-      <AnalyticsVercel />
     </ThemeProvider>
   )
 }
